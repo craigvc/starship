51187fe4ccd6077928cd09fcefbbd0add8f7ff3f
Craig von Chamier <craig@cvcwebsolutions.com>
2024-05-14 12:46:40 +0200

    Pages

--- Modules/CompaniesJetbrainsIntellijGetEnvironment/resources/views/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('companiesjetbrainsintellijgetenvironment::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('companiesjetbrainsintellijgetenvironment.name') !!}</p>
-@endsection
